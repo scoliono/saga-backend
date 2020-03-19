22ffe6ae32fed016754c1a62ef1e3860259f5de7
James Shiffer <2191476+scoliono@users.noreply.github.com>
2020-03-19 19:00:25 +0000

    Route email verification through frontend

--- config/app.php
+++ config/app.php
@@ -64,7 +64,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost:8000'),
+    'frontend_url' => env('APP_FRONTEND_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
